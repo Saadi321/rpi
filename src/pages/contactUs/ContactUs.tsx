0ba3092c88effea7dcea9c6b6a0abec9f6eadfa1
Muhammad Saad Satti <saadienterprises1122@gmail.com>
2025-11-27 08:47:06 +0500

    Updated Contact Page

--- src/pages/contactUs/ContactUs.tsx
+++ src/pages/contactUs/ContactUs.tsx
@@ -1,8 +1,15 @@
-import { Contact } from '@/components'
-import React from 'react'
+import { Contact, DynamicHero } from '@/components'
 
 export const ContactUs = () => {
     return (
-        <div><Contact /></div>
+        <div>
+            <DynamicHero
+                title="Get in"
+                subtitle="Touch"
+                description="Rawalpindi Polytechnic Institute is a premier seat of learning, dedicated to producing skilled professionals who drive the nation's industrial growth."
+                badge="Since 1995"
+            />
+            <Contact />
+        </div>
     )
 }
